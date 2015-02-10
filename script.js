5b2865a28db8d4a1be3c009180c08a5a3a1472c4
Albert Sison <asison89@gmail.com>
2015-02-09 22:37:17 -0800

    add color scheme

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var bardata = [20, 30, 20, 15, 40, 200,20, 15, 40, 200,20, 15, 40, 200];
+var bardata = [20, 30, 70, 15, 10, 80, 20, 15, 40, 90,20, 15, 40, 125];
+
+for (var i = 0; i< 50; i++) {
+	bardata.push(Math.random()* 30)
+}
+
 
 var height = 400,
 		width = 600,
 		barWidth = 50,
 		barOffset = 5;
+
+
+var colors = d3.scale.linear()
+				.domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
+				.range(['#FFB832', '#C61C6F', '#268BD2', '#85992C'])
 
 var yScale = d3.scale.linear()
 				.domain([0, d3.max(bardata)])
@@ -14,13 +24,16 @@ var xScale = d3.scale.ordinal()
 				.rangeBands([0, width])
 
 
+
 d3.select('#chart').append('svg')
 		.attr('width', width)
 		.attr('height', height)
 		.style('background', '#C9D7D6')
 		.selectAll('rect').data(bardata)
 		.enter().append('rect')
-				.style('fill', '#C61C6F')
+				.style('fill', function(d,i){
+					return colors(i);
+				})
 				.attr('width', xScale.rangeBand())
 				.attr('height', function(d) {
 					return yScale(d);
