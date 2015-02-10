449ec451ced0370478a30164f1883710d8be4e91
Albert Sison <asison89@gmail.com>
2015-02-09 22:51:39 -0800

    add color events

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var height = 400,
 		barWidth = 50,
 		barOffset = 5;
 
+var tempColor;
+
 
 var colors = d3.scale.linear()
 				.domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
@@ -44,3 +46,18 @@ d3.select('#chart').append('svg')
 				.attr('y', function(d) {
 					return height - yScale(d);
 				})
+
+		.on('mouseover', function(d) {
+			tempColor = this.style.fill;
+			d3.select(this)
+					.style('opacity', .5)
+					.style('fill', 'yellow')
+		})
+
+		.on('mouseout', function(d) {
+			d3.select(this)
+				.style('opacity', 1)
+				.style('fill', tempColor)
+		})
+
+
